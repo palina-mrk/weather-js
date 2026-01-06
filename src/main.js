c61a0eb204978105d1fc2a7b603d9c8a9da7109c
Polina <polina.hudyakova@onlyoffice.com>
2026-01-06 13:33:07 +0300

    try ymap

--- src/main.js
+++ src/main.js
@@ -6,3 +6,27 @@ const rootEl = document.getElementById('app');
 rootEl.append(staticElements.createHeader());
 rootEl.append((new MainTag).getElement());
 rootEl.append(staticElements.createFooter());
+
+async function changeMap(lon, lat) {
+  if(!ymaps3) {
+    console.log('error');
+    return;
+  }
+  await ymaps3.ready;
+
+  const { YMap, YMapDefaultSchemeLayer } = ymaps3;
+
+  if (document.getElementById("map").firstElementChild)
+    document.getElementById("map").firstElementChild.remove();
+
+  const map = new YMap(document.getElementById("map"), {
+    location: {
+      center: [lon, lat],
+      zoom: 10,
+    },
+  });
+
+  map.addChild(new YMapDefaultSchemeLayer());
+}
+
+changeMap(23,23);
