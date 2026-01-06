fc9319a80f8df048205e4673215cf10f62cdeecb
Polina <polina.hudyakova@onlyoffice.com>
2026-01-06 13:54:50 +0300

    try ymap

--- src/main.js
+++ src/main.js
@@ -7,15 +7,6 @@ rootEl.append(staticElements.createHeader());
 rootEl.append((new MainTag).getElement());
 rootEl.append(staticElements.createFooter());
 
-function getMap() {
-  const srcEl = document.createElement('srcipt');
-  srcEl.setAttribute('src', 'https://api-maps.yandex.ru/v3/?apikey=6f130378-8e07-4626-a413-392271cdb214&lang=ru_RU');
-  srcEl.setAttribute('defer', true);
-  document.querySelector('head').append(srcEl);
-  srcEl.onload(() => console.log('loaded!'));
-};
-getMap();
-/*
 async function changeMap(lon, lat) {
   if(!ymaps3) {
     console.log('error');
@@ -38,4 +29,4 @@ async function changeMap(lon, lat) {
   map.addChild(new YMapDefaultSchemeLayer());
 }
 
-changeMap(23,23);*/
+changeMap(23,23);
